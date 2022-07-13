77f00e9c7341233798dc34a9bce60b774076d671
Aries Hoang <hathanhhoang842000@gmail.com>
2022-07-13 08:32:36 +0700

    Ex6 Attack monster

--- Day05/Draft2/app.js
+++ Day05/Draft2/app.js
@@ -2,12 +2,16 @@ const app = Vue.createApp({
     data() {
         return { 
             enteredGoalValue: '',
-            goals: [] 
+            goals: []
         };
     },
     methods: {
         addGoal() {
             this.goals.push(this.enteredGoalValue);
+            this.enteredGoalValue = ''
+        },
+        removeGoal(index) {
+            this.goals.splice(index, 1)
         }
     },
 });
